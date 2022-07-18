d76a30d80b00037419f2b58fa6fb1cfb5edae49b
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-07-18 14:35:46 +0200

    added cors as middleware to app in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const address: string = '0.0.0.0:3000'
 const port = process.env.SERVER_PORT
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', function (req: Request, res: Response) {
-	res.send('Hello World!')
+	res.send('server running!')
 })
 
 app.listen(port, function () {
