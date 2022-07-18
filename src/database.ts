8acde46668278524e5b179eb66952caf986a825d
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-07-18 13:46:04 +0200

    added database.json and test database to env file

--- src/database.ts
+++ src/database.ts
@@ -3,12 +3,20 @@ import { Pool } from 'pg'
 
 dotenv.config()
 
-const { POSTGRES_URL, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-	process.env
+const {
+	POSTGRES_URL,
+	POSTGRES_DB,
+	POSTGRES_TEST_DB,
+	POSTGRES_USER,
+	POSTGRES_PASSWORD,
+	ENV,
+} = process.env
+
+console.log('postgres db in database.ts: ', POSTGRES_DB)
 
 const client = new Pool({
 	host: POSTGRES_URL,
-	database: POSTGRES_DB,
+	database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
 	user: POSTGRES_USER,
 	password: POSTGRES_PASSWORD,
 })
