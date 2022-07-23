d33c62f21bbf45bedd46555143533c2c96196637
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-07-23 16:32:38 +0200

    changed docker-compose, changed test script, ENV => NODE_ENV

--- src/database.ts
+++ src/database.ts
@@ -11,14 +11,14 @@ const {
 	POSTGRES_TEST_DB,
 	POSTGRES_USER,
 	POSTGRES_PASSWORD,
-	ENV,
 } = process.env
 
-console.log('ENV in database.ts: ', ENV)
+let NODE_ENV
+console.log('node_env in database.ts: ', NODE_ENV)
 
 const client = new Pool({
 	host: POSTGRES_URL,
-	database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_TEST_DB,
+	database: NODE_ENV === 'test' ? POSTGRES_TEST_DB : POSTGRES_DB,
 	user: POSTGRES_USER,
 	password: POSTGRES_PASSWORD,
 })
